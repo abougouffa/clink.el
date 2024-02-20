ccdac815d5d9e64395bd4180dd2ce83713d0e509
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2024-02-20 19:03:20 +0100

    chore(version): v0.4

--- clink.el
+++ clink.el
@@ -5,7 +5,7 @@
 ;; Author: Abdelhak BOUGOUFFA (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Maintainer: Abdelhak BOUGOUFFA (rot13 "nobhtbhssn@srqbencebwrpg.bet")
 ;; Created: 2024
-;; Version: 0.3
+;; Version: 0.4
 ;; Package-Requires: ((emacs "28.1"))
 ;; Homepage: https://github.com/abougouffa/clink.el
 ;; Keywords: matching, files, completion, languages, c, tools
